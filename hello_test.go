001c999b5d4056cc733809b2c428f3655700e2dc
kupacariibumu <108216704+kupacariibumu@users.noreply.github.com>
2023-04-21 15:25:18 +0700

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "teating"
 
 func HelloTest(t *testing.T) {
     expected := "Hello Hai"
-    if ret := hello(); ret != expected {
-      t.Errorf("hello() = %q", ret)
+    if ret := Hello(); ret != expected {
+      t.Errorf("hello() = %q want %q", ret, )
     }
 }
